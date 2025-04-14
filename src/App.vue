f1652f1b106311b2a23c67a57a3521047a398eb1
Mirov <mirov0727@gmail.com>
2025-04-14 20:46:25 +0800

    adding transition and rest functionality

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <template>
   <div class="ctr">
-    <questions 
-      v-if="questionsAnswered < questions.length" 
-      :questions="questions"
-      :questionsAnswered="questionsAnswered"
-      @question-answered="questionAnswered"
-    />
-    <result v-else/>
-
-    <button type="button" class="reset-btn">Reset</button>
+    <transition name="fade" mode="out-in">
+      <questions 
+        v-if="questionsAnswered < questions.length" 
+        :questions="questions"
+        :questionsAnswered="questionsAnswered"
+        @question-answered="questionAnswered"
+      />
+      <result v-else :results="results" :totalCorrect="totalCorrect"/>
+    </transition>
+    
+    <button 
+      type="button" 
+      class="reset-btn" 
+      @click.prevent="reset" 
+      v-if="this.questionsAnswered === questions.length"
+    >
+      Reset
+    </button>
   </div>
 </template>
 
 <script>
 import Questions from "./components/Questions.vue";
 import Result from "./components/Result.vue";
- 
+
 export default {
   name: 'App',
   components: {
@@ -109,7 +118,11 @@ export default {
         this.totalCorrect++;
       }
       this.questionsAnswered++;
-    }
+    },
+    reset() {
+      this.questionsAnswered = 0;
+      this.totalCorrect = 0;
+    },
   }
 }
 </script>
